54340e3169a974cb593061ca275eb72b81f08bd6
oliver <olivercastroteran@gmail.com>
2023-10-16 20:36:38 -0400

    Day 30 - Finished

---
+++ 11-javascript-basics/day-30_32/scipt.js
@@ -0,0 +1,11 @@
+let age = 32;
+let userName = 'Oliver';
+let hobbies = ['Code', 'Climbing', 'Reading'];
+let job = {
+  title: 'Full Stack Developer',
+  place: 'London',
+  salary: 50000,
+};
+
+// alert(hobbies[0]);
+alert(job.salary);
