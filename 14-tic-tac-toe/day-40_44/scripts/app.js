75fd82caf14bb66a214003e340772814a541a982
oliver <olivercastroteran@gmail.com>
2023-10-24 02:26:13 -0400

    Day 42 - Finished

--- 14-tic-tac-toe/day-40_44/scripts/app.js
+++ 14-tic-tac-toe/day-40_44/scripts/app.js
@@ -1,12 +1,29 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: '',
+    symbol: 'X',
+  },
+  {
+    name: '',
+    symbol: 'O',
+  },
+];
+
 const playerConfigOverlay = document.getElementById('config-overlay');
 const backdrop = document.getElementById('backdrop');
+const form = document.querySelector('form');
+const errorsOutput = document.getElementById('config-errors');
 
 const edtitPlayer1Btn = document.getElementById('edit-player-1-btn');
 const edtitPlayer2Btn = document.getElementById('edit-player-2-btn');
 const cancelConfigBtn = document.getElementById('cancel-config-btn');
 
-edtitPlayer1Btn.addEventListener('click', openPlayeConfig);
-edtitPlayer2Btn.addEventListener('click', openPlayeConfig);
+edtitPlayer1Btn.addEventListener('click', openPlayerConfig);
+edtitPlayer2Btn.addEventListener('click', openPlayerConfig);
 
-cancelConfigBtn.addEventListener('click', closePlayeConfig);
-backdrop.addEventListener('click', closePlayeConfig);
+cancelConfigBtn.addEventListener('click', closePlayerConfig);
+backdrop.addEventListener('click', closePlayerConfig);
+
+form.addEventListener('submit', savePlayerConfig);
