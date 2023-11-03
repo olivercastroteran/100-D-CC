99d9d281a109ab9af938a6eb16cb841ec844c5c9
oliver <olivercastroteran@gmail.com>
2023-11-03 08:01:44 -0400

    Day 66 - Finished

--- 23-nodejs-mongodb/day-65_66/routes/blog.js
+++ 23-nodejs-mongodb/day-65_66/routes/blog.js
@@ -11,8 +11,13 @@ router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function (req, res) {
-  res.render('posts-list');
+router.get('/posts', async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection('posts')
+    .find({}, { title: 1, summary: 1, 'author.name': 1 })
+    .toArray();
+  res.render('posts-list', { posts });
 });
 
 router.post('/posts', async (req, res) => {
@@ -42,6 +47,76 @@ router.post('/posts', async (req, res) => {
 router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
   res.render('create-post', { authors });
+});
+
+router.get('/posts/:id', async (req, res, next) => {
+  let postId = req.params.id;
+
+  try {
+    postId = req.params.id;
+  } catch (error) {
+    // return res.status(404).render('404');
+    return next(error);
+  }
+  const post = await db
+    .getDb()
+    .collection('posts')
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+
+  if (!post) {
+    return res.status(404).render('404');
+  }
+
+  post.humanReadableDate = post.date.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+  post.date = post.date.toISOString();
+
+  res.render('post-detail', { post });
+});
+
+router.get('/posts/:id/edit', async (req, res) => {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection('posts')
+    .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+  if (!post) {
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post', { post });
+});
+
+router.post('/posts/:id/edit', async (req, res) => {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection('posts')
+    .updateOne(
+      { _id: postId },
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+          // date: new Date()
+        },
+      }
+    );
+
+  res.redirect('/posts');
+});
+
+router.post('/posts/:id/delete', async (req, res) => {
+  const postId = new ObjectId(req.params.id);
+  await db.getDb().collection('posts').deleteOne({ _id: postId });
+
+  res.redirect('/posts');
 });
 
 module.exports = router;
